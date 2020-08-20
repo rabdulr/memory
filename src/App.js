1a30adbe29be4db8f81985aa5a22f0d5d7517aa9
Redzuan <rabdulr@icloud.com>
2020-08-20 16:13:48 -0700

    Tweaks

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h3>Memory</h3>
+            <hr/>
+        </div>
+    )
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 const root = document.querySelector('#root');
-const App = () => {
-    return (
-        <div>
-            <h3>Hello!</h3>
-            <hr/>
-        </div>
-    )
-}
 
 ReactDOM.render(<App />, root);
