924ad16ca847350b1fe7676fbc7a5097a253b395
Redzuan <rabdulr@icloud.com>
2020-08-20 16:56:54 -0700

    added Tone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
+import tone from 'tone';
 
 const App = () => {
+    const [isLoaded, setLoaded] = useState(false);
+    const squares = ['C4', 'C#4', 'D4']
+    // const synth = new Tone;
+    // synth.oscillator.type = "sine";
+    // synth.toMaster();
+    const key = useRef();
+    console.log('Tone', tone)
+
     return (
         <div>
             <h3>Memory</h3>
             <hr/>
+            <div id='squares'>
+                <ul>
+                    { squares.map(square => {
+                        return (
+                            <li data-note={`${square}`} className='square' key={`${square}`} onClick={() => console.log(key.current)}>Click me!</li>
+                        )
+                    })}
+
+                </ul>
+            </div>
         </div>
     )
 };
