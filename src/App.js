d78ec3194fa3312ab28d353ca84d61077f253edb
Redzuan <rabdulr@icloud.com>
2020-08-21 13:29:39 -0700

    Testing clicks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React, {useRef, createRef, useState, useEffect} from 'react';
 import * as Tone from 'tone';
 
 const App = () => {
-    const [count, setCount] = useState(0)
-    const squares = ['C4', 'C#4', 'D4']
+    const squares = ['C4', 'C#4', 'D4'];
+    let inputRef = useRef(squares.map(createRef));
     const synth = new Tone.Synth();
     synth.oscillator.type = "sine";
-    synth.toMaster();
+    synth.toDestination();
+    console.log(inputRef.current)
 
+    const handleClick = (event, idx) => {
+        // inputRef.current[idx].value = `Hello ${idx}!`
+        const {current} = inputRef.current[idx];
+        // console.log( current.dataset.note )
+        synth.triggerAttack(current.dataset.note)
+        setInterval(() => {
+            synth.triggerRelease();
+        }, 500)
+    }
 
     return (
         <div>
             <h3>Memory</h3>
             <hr/>
             <div id='squares'>
+                <button onClick={randomPlay}>Random Play of Array</button>
+                <h5>Array Count: {squares.length} </h5>
                 <ul>
-                    { squares.map(square => {
+                    { squares.map((square, idx) => {
                         return (
                             <li 
                                 data-note={`${square}`} 
-                                className='square' 
+                                className={`square${idx}`} 
                                 key={`${square}`} 
-                                >{square}
+                                ref={inputRef.current[idx]}
+                                value={square}
+                                onClick={(event) => handleClick(event, idx)}
+                                >
+                                    {square}
                             </li>
                         )
                     })}
